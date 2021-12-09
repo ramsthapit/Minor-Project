64fc10f2f320e500b00e5354164c2372a4a9f198
Ram Sthapit <ramsthapit50@gmail.com>
2021-12-09 18:54:24 +0545

    React initial

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main></main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
