919e13db0afa46030a2571e715ceaf8bceb98252
Ram Sthapit <ramsthapit50@gmail.com>
2021-12-09 19:45:21 +0545

    React-Bootstrap-setup

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Footer = () => {
   return (
     <footer>
-      
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; Book Finder
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      
+      <Navbar bg="light" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Book Finder</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/"><i className='fas fa-home'></i>Home</Nav.Link>
+              <Nav.Link href="/freeEbook"><i className='fas fa-book'></i>Free Ebook</Nav.Link>
+              <NavDropdown title="Search" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/">Book Search</NavDropdown.Item>
+                <NavDropdown.Item href="/">Frequrntly Downloaded</NavDropdown.Item>
+                <NavDropdown.Item href="/">Book Shelves</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="/">Offline catelog</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+              <Button variant="success" size='lg' >Donate</Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
