c8ebe07d36fc62ed5f7fc07563137f5882d71a2f
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-13 10:51:40 +0545

    Static Files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,6 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+
+STATICSFILES_DIRS=[
+    BASE_DIR/'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
