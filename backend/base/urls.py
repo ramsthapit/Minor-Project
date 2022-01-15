9b6accfad631c8f8157ecff2b764e069f9af2603
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-15 15:02:08 +0545

    user serializers

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(),
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
         name='token_obtain_pair'),
     path('', views.getRoutes,name="routes"),
+    path('users/profile/', views.getUserProfile, name="user-profile"),
     path('books/', views.getBooks,name="books"),
     path('book/<str:pk>/', views.getBook,name="book"),
 ] 
--- backend/base/views.py
+++ backend/base/views.py
@@ -6,9 +6,24 @@ from rest_framework.response import Response
 
 from .books import books
 from .models import Book
-from .serializers import BookSerializer
+from .serializers import BookSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+      data = super().validate(attrs)
+
+      serializers = UserSerializerWithToken(self.user).data
+      for k, v in serializers.items():
+        data[k]=v
+
+      return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -23,6 +38,12 @@ def getRoutes(request):
     '/api/books/<update>/<id>/',
   ]
   return Response(routes)
+
+@api_view(['GET'])
+def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
 
 @api_view(['GET'])
 def getBooks(request):
