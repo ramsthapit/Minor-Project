bc9b9fb25c05f5b7962865afb29cd6c34358a70b
Ram Sthapit <ramsthapit50@gmail.com>
2022-01-15 10:51:34 +0545

    WebToken installation and setup

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(),
+        name='token_obtain_pair'),
     path('', views.getRoutes,name="routes"),
     path('books/', views.getBooks,name="books"),
     path('book/<str:pk>/', views.getBook,name="book"),
-]
+] 
